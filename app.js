65e42dacf60f7e38f514b774838e59ebc36a1d54
BUMBALORD <davidhallinan@gmail.com>
2018-02-01 14:20:00 -0800

    add files

---
+++ app.js
@@ -0,0 +1,27 @@
+$(function(){
+
+  function addGoogleFont(fontName) {
+    fontName.toUpperCase();
+    $("head").append("<link href='https://fonts.googleapis.com/css?family=" + fontName + "' rel='stylesheet' type='text/css'>");
+    $(".exampleText").css('font-family', fontName);
+    var headerFont = "<span class='headerChange'>" + fontName + "</span>"
+    $(".exampleHeader").html("Here's what  " + headerFont + "  looks like in action!");
+    $(".headerChange").css('font-family', fontName);
+  }
+
+  $('#submitFont').on('click', function(event){
+    event.preventDefault();
+    var font = $('#fontInput').val().trim().toLowerCase();
+    // console.log(font);
+    $.ajax({
+    url: 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyADMWRN_nOc1sRCdmybHHgX82pNdSWYh9I',
+    type: 'GET'
+    }).done(function(response){
+      for (var i = 0; i < response.items.length; i++) {
+        if (font === response.items[i].family.toLowerCase()) {
+          addGoogleFont(response.items[i].family);
+        }
+  };
+  })
+})
+})
